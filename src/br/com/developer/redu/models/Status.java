98925c41fbc6d28e4c3054bd4131a9453801c31b
Luis Medeiros <lpmfilho@gmail.com>
2013-03-18 09:22:29 -0300

    Adding attributes on Status model

--- src/br/com/developer/redu/models/Status.java
+++ src/br/com/developer/redu/models/Status.java
@@ -15,17 +15,20 @@ public class Status {
 	public final static String HELP = "Help";
 	public final static String LOG = "Log";
 	
+	public String id;
     public String type;
     public String logeable_type;
     public String created_at;
     public String text;
     public User user;
+    public boolean lectureAreadySeen;
+    public boolean lastSeen;
     public List<Link> links;
 
     @Override
     public String toString(){
-        return String.format("type: %s\nlogeable_type: %s\ncreated_at: %s\ntext: %s\nuser: %s\nlinks: %s",
-                type, logeable_type, created_at,text, user, links);
+        return String.format("id: %s\ntype: %s\nlogeable_type: %s\ncreated_at: %s\ntext: %s\nuser: %s\nlectureAreadySeen: %s\nlastSeen: %s\nlinks: %s",
+                id, type, logeable_type, created_at,text, user, lectureAreadySeen, lastSeen, links);
     }
 
 }
